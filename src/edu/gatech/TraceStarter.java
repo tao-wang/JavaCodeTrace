cbafb9200ea144f003e3b82cc5fb43174092118e
tao-wang <twang@gatech.edu>
2016-04-02 18:31:04 -0400

    Paintable interface

--- src/edu/gatech/TraceStarter.java
+++ src/edu/gatech/TraceStarter.java
@@ -3,6 +3,7 @@ package edu.gatech;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -16,7 +17,7 @@ public class TraceStarter {
         JFrame frame = new JFrame("TraceStarter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        frame.add(new MyPanel());
+        frame.add(new MyCanvas());
         frame.pack();
         frame.setLocationRelativeTo(null);
  
@@ -38,12 +39,21 @@ public class TraceStarter {
 }
 
 @SuppressWarnings("serial")
-class MyPanel extends JPanel {
+class MyCanvas extends JPanel {
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
 	
-	public MyPanel() {
+	ArrayList<Paintable> objects;
+	
+	public MyCanvas() {
         setBorder(BorderFactory.createLineBorder(Color.black));
+        objects = new ArrayList<Paintable>();
+        MyTextBox box1 = new MyTextBox("Hello, World. I'm Tao!", 20, 30);
+        MyTextBox box2 = new MyTextBox("Foo bar baz qux", 400, 300);
+        MyArrow arrow1 = new MyArrow(box1, box2);
+        objects.add(box1);
+        objects.add(box2);
+        objects.add(arrow1);
     }
 
     public Dimension getPreferredSize() {
@@ -54,6 +64,73 @@ class MyPanel extends JPanel {
         super.paintComponent(g);       
 
         // Draw Text
-        g.drawString("This is my custom Panel!",10,20);
+        for (Paintable p : objects)
+        {
+        	p.paint(g);
+        }
     }  
+}
+
+class MyTextBox implements Paintable {
+	
+	public int x;
+	public int y;
+	public int width;
+	public int height;
+	public String text;
+	
+	public MyTextBox(String text, int x, int y)
+	{
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.width = 8 * text.length();
+		this.height = 20;
+	}
+	
+	public void paint(Graphics g)
+	{
+		g.drawRect(x, y, width, height);
+		g.drawString(text, x+4, y+15);
+	}
+	
+}
+
+class MyArrow implements Paintable {
+
+	private int startX;
+	private int startY;
+	private int endX;
+	private int endY;
+	
+	public MyArrow(int x1, int y1, int x2, int y2)
+	{
+		startX = x1;
+		startY = y1;
+		endX = x2;
+		endY = y2;
+	}
+	
+	public MyArrow(MyTextBox one, MyTextBox two)
+	{
+		MyTextBox left = one;
+		MyTextBox right = two;
+		
+		if (one.x > two.x)
+		{
+			left = two;
+			right = one;
+		}
+		
+		startX = left.x + left.width;
+		startY = left.y + left.height/2;
+		endX = right.x;
+		endY = right.y + right.height/2;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawLine(startX, startY, endX, endY);
+	}
+	
 }
